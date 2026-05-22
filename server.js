1b57a5a1e1626b84af99aca7fb774b0204596f54
Ronald <ronald.escobar.v.479@gmail.com>
2026-05-22 01:02:16 -0400

    Se agrega ruta GET para tareas

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.static("public"));
 
+let tareas = [];
+
+app.get("/tareas", function(req, res){
+  res.json(tareas);
+});
+
 app.listen(3000, () => {
   console.log("Servidor funcionando");
 });
